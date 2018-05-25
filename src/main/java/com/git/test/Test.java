eb5b9015124aa7724e0fa59d857751d983e3e870
kaizhao3 <kaizhao3@iflytek.com>
2018-05-25 15:44:01 +0800

    测试提交

---
+++ src/main/java/com/git/test/Test.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+/**
+ * @author kaizhao3
+ * @ClassName: Test
+ * @Description:
+ * @date 2018/5/25、15:43
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("--------------");
+    }
+}

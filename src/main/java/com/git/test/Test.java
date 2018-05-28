900393f64fc053030aa1cfc30f958fa78b297839
kaizhao3 <kaizhao3@iflytek.com>
2018-05-28 20:28:25 +0800

    功能合并

--- src/main/java/com/git/test/Test.java
+++ src/main/java/com/git/test/Test.java
@@ -8,7 +8,11 @@ package com.git.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("--------------");
-        System.out.println("sdsds");
+        System.out.println("每天都要down一次最新的代码");
+        System.out.println("我准备添加一个新功能1了，重新开一个Git工作流---新的功能分支");
+        System.out.println("我要添加一个新功能1------");
+        System.out.println("功能1添加成功了------");
+        System.out.println("功能1添加成功了  准备合并分支------");
+
     }
 }
